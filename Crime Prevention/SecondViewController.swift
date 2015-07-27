20e835d756982d019f54e4c21b8fa4fef32fb994
Irene Hwang <irenehwang@Irenes-MacBook-Air.local>
2015-07-27 14:01:12 -0700

    Initial Commit

---
+++ Crime Prevention/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Crime Prevention
+//
+//  Created by Irene Hwang on 7/27/15.
+//  Copyright (c) 2015 Irene Hwang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
